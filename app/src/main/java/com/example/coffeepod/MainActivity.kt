e6d1f42f88976a7aba1a214bab9da3c8fee2b666
Blake Hakkila <blake.austin.hakkila@gmail.com>
2022-04-09 19:16:45 -0700

    added main fragments and navigation for review feed, review creation, and user profile

--- app/src/main/java/com/example/coffeepod/MainActivity.kt
+++ app/src/main/java/com/example/coffeepod/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.example.coffeepod
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.coffeepod.fragments.ComposeFragment
+import com.example.coffeepod.fragments.FeedFragment
+import com.example.coffeepod.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            item ->
+
+            var fragmentToShow: Fragment? = null
+
+            when (item.itemId) {
+                R.id.action_home -> {
+                    fragmentToShow = FeedFragment()
+                }
+
+                R.id.action_compose -> {
+                    fragmentToShow = ComposeFragment()
+                }
+
+                R.id.action_profile -> {
+                    fragmentToShow = ProfileFragment()
+                }
+            }
+
+            if (fragmentToShow != null) {
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+            }
+
+            true
+        }
+
+        bottomNavigationView.selectedItemId = R.id.action_home
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_logout) {
+            Log.i("Main", "Logout")
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
