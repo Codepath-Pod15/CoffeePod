faccc1719eb8b8de2b6e25265895aa64d85cac32
crowleyg <crowleyg@oregonstate.edu>
2022-04-19 14:22:13 -0700

    updated action bar color added scrollview to compose screen added placeholder image to compose screen modified item_post.xml modified item_review.xml for symmetry with item_post.xml

--- app/src/main/java/com/example/coffeepod/MainActivity.kt
+++ app/src/main/java/com/example/coffeepod/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.coffeepod
 
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.coffeepod.fragments.ComposeFragment
@@ -16,9 +18,11 @@ import com.parse.ParseUser
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.backgroundColor)))
 
         val fragmentManager: FragmentManager = supportFragmentManager
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
